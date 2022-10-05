1138bf9768bde12bd01c719acb96cae767919151
naalt0 <88773094+naalt0@users.noreply.github.com>
2022-10-05 13:13:30 +0300

    Add files via upload

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ $username = "db_projekti";
 $password = "Sivyh2022";
 $dbname = "chat";
  
-/*$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn){
   echo "Database connection error".mysqli_connect_error();
-}*/
+}
 ?>
