c968b349936d0b0be50ac86bf9c3210ef4e6bded
naalt0 <88773094+naalt0@users.noreply.github.com>
2022-10-19 19:26:58 +0300

    Add files via upload

--- landing.php
+++ landing.php
@@ -2,11 +2,11 @@
 session_start();
 include("config.php");
 
-if(isset($_SESSION['userID'])){
+if(isset($_SESSION['contactID'])){
     header("location: login.php");
 }
 
-$sql = mysqli_query($conn, "SELECT * FROM User WHERE userID = {$_SESSION['userID']}");
+$sql = mysqli_query($conn, "SELECT * FROM Contact WHERE user1 = {$_SESSION['contactID']}");
 if(mysqli_num_rows($sql) > 0){
     $row = mysqli_fetch_assoc($sql);
 }
@@ -21,10 +21,10 @@ if(mysqli_num_rows($sql) > 0){
       <header>
         <div class="content">
           <div class="details">
-            <span><?php echo $row['username'] ?></span>
+            <span><?php echo $row['user1'] ?></span>
           </div>
         </div>
-        <a href="php/logout.php?logout_id=<?php echo $row['userID']; ?>" class="logout">Logout</a>
+        <a href="php/logout.php?logout_id=<?php echo $row['user1']; ?>" class="logout">Logout</a>
       </header>
       <div class="search">
         <span class="text">Select an user to start chat</span>
