eb5c071ca421ae12724f9a76203d8432112c3368
naalt0 <88773094+naalt0@users.noreply.github.com>
2022-10-28 03:21:49 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "hyvis.mysql.database.azure.com";
-$username = "db_projekti";
-$password = "Sivyh2022";
-$dbname = "chat";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if($conn->connect_error){
